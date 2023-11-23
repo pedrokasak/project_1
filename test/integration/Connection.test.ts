ca4f03f284ba7628c5f005d0df168e8e727f5f49
Pedro Henrique <pedrohesm@gmail.com>
2023-11-23 20:43:03 -0300

    test: create test connection db

--- test/integration/Connection.test.ts
+++ test/integration/Connection.test.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 test("Must be connection database prisma", async function () {
   const prisma = new PrismaClient()
-  const itemsData = await prisma.item.findMany()
+  const itemsData = await prisma.$connect;
   expect(itemsData).toBeTruthy();
 });
